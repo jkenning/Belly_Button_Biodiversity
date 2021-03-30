72794d1cfb9ce50e005b541c80321ab7a6974fef
Jack Kenning <jack.j.kenning@gmail.com>
2021-03-29 20:44:13 -0500

    Update charts.js

--- charts.js
+++ charts.js
@@ -73,24 +73,29 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
     
-    var yticks = otu_ids.slice(0,10).map(otuID => "OTU ${otuID}").reverse();
+    var yticks = otu_ids.slice(0, 10).map(otuID => "OTU ${otuID}").reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = {
-        x: sample_values.slice(0,10).reverse(),
+    var trace1 = {
+        x: sample_values.slice(0, 10).reverse(),
         y: yticks,
-        text: otu_labels.slice(0,10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        name: "Belly",
         type: "bar",
         orientation: "h"
     };
+    var barData = [trace1];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     title: "Top 10 Bacterial Species (OTUs)",
-     yaxis: "OTU id",
-     xaxis: "Sample Value",
-     margin: {t: 100, b: 100, l: 100, r: 100}
+      title: "Top 10 Bacterial Species (OTUs)",
+      margin: {
+        l: 100, 
+        r: 100, 
+        t: 100, 
+        b: 100
+      }
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("plotArea", barData, barLayout);
+    Plotly.newPlot("plot", barData, barLayout);
   });
 }
