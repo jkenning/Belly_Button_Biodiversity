7a59bc3e4aece4c02004fbff26b2eceb7e681b1f
Jack Kenning <jack.j.kenning@gmail.com>
2021-03-29 22:13:37 -0500

    Update files

--- static/js/charts.js
+++ static/js/charts.js
@@ -77,13 +77,14 @@ function buildCharts(sample) {
     // var yticks = otu_ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [{
+    var trace1 = {
       x: sample_values.slice(0,10).reverse(),
       y: yticks,
-      text: otu_labelsslice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
       type: "bar",
       orientation: "h"
-    }];
+    };
+    var barData = [trace1];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacterial Species (OTUs)",
